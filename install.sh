13a9a48ab009d3b9f93d8420aa5a90aad7f834fc
Doniyor Norqulov <pubgkrafton1243@gmail.com>
2022-11-13 21:18:34 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,11 +38,11 @@ printf "\n\e[1;35;47m                                   \e[0m"
 printf "\n\e[1;35;47m          █ █ █           \e[0m"
 printf "\n\e[1;35;47m          █▀█ █            \e[0m"
 printf "\n\e[1;35;47m                   \e[0m"
-printf "\n\n\e[3;34;40m Installing...\e[0m\n\n"
+printf "\n\n\e[3;34;40m O‘rnatilmoqda...\e[0m\n\n"
 
 ##############################################################################
 
-printf "\r\033[0;34mPreparing for installation...\e[0m"
+printf "\r\033[0;34mO‘rnatishga tayyorgarlik ko'rilmoqda...\e[0m"
 
 touch premium-install.log
 if [ ! x"$SUDO_USER" = x"" ]; then
@@ -51,7 +51,7 @@ fi
 
 if [ -d "Premium/premium" ]; then
 	cd Premium || {
-		printf "\rError: Install git package and re-run installer"
+		printf "\rXato: Git paketini o'rnating va o'rnatuvchini qayta ishga tushiring"
 		exit 6
 	}
 	DIR_CHANGED="yes"
@@ -62,7 +62,7 @@ if [ -f ".setup_complete" ]; then
 	if echo "$OSTYPE" | grep -qE '^linux-gnu.*'; then
 		PYVER="3"
 	fi
-	printf "\rExisting installation detected"
+	printf "\rMavjud o'rnatish aniqlandi"
 	clear
 	"python$PYVER" -m premium "$@"
 	exit $?
@@ -72,7 +72,7 @@ fi
 
 ##############################################################################
 
-echo "Installing..." >premium-install.log
+echo "Oʻrnatilmoqda..." >premium-install.log
 
 if echo "$OSTYPE" | grep -qE '^linux-gnu.*' && [ -f '/etc/debian_version' ]; then
 	PKGMGR="apt install -y"
@@ -100,13 +100,13 @@ fi
 ##############################################################################
 
 runout "$SUDO_CMD $PKGMGR python$PYVER" git || {
-	errorout "Core install failed."
+	errorout "Asosiy oʻrnatish amalga oshmadi."
 	exit 2
 }
 
 
-printf "\r\033[K\033[0;32mPreparation complete!\e[0m"
-printf "\n\r\033[0;34mInstalling linux packages...\e[0m"
+printf "\r\033[K\033[0;32mTayyorgarlik tugallandi!\e[0m"
+printf "\n\r\033[0;34mLinux paketlarini o‘rnatilmoqda...\e[0m"
 
 if echo "$OSTYPE" | grep -qE '^linux-gnu.*'; then
 	runout "$SUDO_CMD $PKGMGR python$PYVER-dev"
@@ -122,8 +122,8 @@ fi
 
 runout "$SUDO_CMD $PKGMGR neofetch dialog"
 
-printf "\r\033[K\033[0;32mPackages installed!\e[0m"
-printf "\n\r\033[0;34mCloning repo...\e[0m"
+printf "\r\033[K\033[0;32mPaketlar o‘rnatilgan!\e[0m"
+printf "\n\r\033[0;34mRepo klonlanmoqda...\e[0m"
 
 
 ##############################################################################
@@ -132,31 +132,31 @@ printf "\n\r\033[0;34mCloning repo...\e[0m"
 ${SUDO_CMD}rm -rf Premium
 # shellcheck disable=SC2086
 runout ${SUDO_CMD}git clone https://github.com/pubgcrafton/Premium/ || {
-	errorout "Clone failed."
+	errorout "Klonlash amalga oshmadi."
 	exit 3
 }
 cd Premium || {
-	printf "\r\033[0;33mRun: \033[1;33mpkg install git\033[0;33m and restart installer"
+	printf "\r\033[0;33mRun: \033[1;33mpkg install git\033[0;33m va restart installer"
 	exit 7
 }
 
-printf "\r\033[K\033[0;32mRepo cloned!\e[0m"
-printf "\n\r\033[0;34mInstalling python dependencies...\e[0m"
+printf "\r\033[K\033[0;32mRepo klonlangan!\e[0m"
+printf "\n\r\033[0;34mPython bog‘liqliklarini o‘rnatilmoqda...\e[0m"
 
 # shellcheck disable=SC2086
 runin "$SUDO_CMD python$PYVER" -m pip install --upgrade pip setuptools wheel --user
 # shellcheck disable=SC2086
 runin "$SUDO_CMD python$PYVER" -m pip install -r requirements.txt --upgrade --user --no-warn-script-location --disable-pip-version-check || {
-	errorin "Requirements failed!"
+	errorin "Talablar bajarilmadi!"
 	exit 4
 }
 rm -f ../premium-install.log
 touch .setup_complete
 
-printf "\r\033[K\033[0;32mDependencies installed!\e[0m"
-printf "\n\033[0;32mStarting...\e[0m\n\n"
+printf "\r\033[K\033[0;32mBog‘liqliklar o‘rnatilgan!\e[0m"
+printf "\n\033[0;32mBoshlanmoqda...\e[0m\n\n"
 
 ${SUDO_CMD}"python$PYVER" -m premium "$@" || {
-	printf "\033[1;31mPython scripts failed\e[0m"
+	printf "\033[1;31mPython skriptlari muvaffaqiyatsiz tugadi\e[0m"
 	exit 5
 }
